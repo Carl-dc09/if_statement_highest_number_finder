674795106264d304fdf0a966bd293d48004cce89
unknown <diamantecarl9@gmail.com>
2024-10-15 02:48:07 +0800

    added a if statement

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -8,4 +8,5 @@ fourth_num_value = input("Fourth value:")
 fifth_num_value = input("Fifth value:")
 
 def fing_highest(first_num_value, sec_num_value, third_num_value, fourth_num_value, fifth_num_value):
+    if first_num_value > sec_num_value:
 
