dd7f89736d3b18417d40699e424dcd37dbd138a5
unknown <diamantecarl9@gmail.com>
2024-10-15 02:47:12 +0800

    added a def

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -6,3 +6,6 @@ sec_num_value = input("Second value:")
 third_num_value = input("Third value:")
 fourth_num_value = input("Fourth value:")
 fifth_num_value = input("Fifth value:")
+
+def fing_highest(first_num_value, sec_num_value, third_num_value, fourth_num_value, fifth_num_value):
+
