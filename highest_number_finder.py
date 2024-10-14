d57a5e926a7dc499a716772f47cb786f301078cb
unknown <diamantecarl9@gmail.com>
2024-10-15 02:54:40 +0800

    added some more

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -13,4 +13,6 @@ def fing_highest(first_num_value, sec_num_value, third_num_value, fourth_num_val
             return "first_num_value"
         else:
             return "third_num_value"
-
+    else:
+        if sec_num_value > third_num_value:
+            return "sec_num_value"
