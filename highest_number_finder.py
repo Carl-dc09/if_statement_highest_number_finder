355194fe4d654b5a45bbf62cb43ab1a45f2e4e0c
unknown <diamantecarl9@gmail.com>
2024-10-15 02:32:14 +0800

    all inputs are working

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -1,4 +1,7 @@
 # Set the input numbers
+
 first_num_value = input("First value:")
 sec_num_value = input("Second value:")
 third_num_value = input("Third value:")
+fourth_num_value = input("Fourth value:")
+fifth_num_value = input("Fifth value:")
