0f221350e5c4dd18c3c7ca4719f0a3108f687969
unknown <diamantecarl9@gmail.com>
2024-10-15 20:06:36 +0800

    Final revised

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -11,19 +11,15 @@ def first_find_highest(first_num_value, sec_num_value, third_num_value):
         else:
             return third_num_value
 
-# For the last two values
-def second_find_highest(fourth_num_value, fifth_num_value):
-    if fourth_num_value > fifth_num_value:
+# To find the most highest
+def final_most_highest(first_highest, fourth_num_value, fifth_num_value):
+    if first_highest > fourth_num_value:
+        if first_highest > fifth_num_value:
+            return first_highest
+        else:
+            return fifth_num_value
+    else:
         return fourth_num_value
-    else:
-        return fifth_num_value
-
-# To compare the two highest number of the two sets
-def final_find_highest(first_highest, second_highest):
-    if first_highest > second_highest:
-        return first_highest
-    else:
-        return second_highest
 
 # Input for all five values
 first_value = int(input("First value: "))
@@ -32,12 +28,11 @@ third_value = int(input("Third value: "))
 fourth_value = int(input("Fourth value: "))
 fifth_value = int(input("Fifth value: "))
 
-# The results for the first and second set
-res1 = first_find_highest(first_value, sec_value, third_value)
-res2 = second_find_highest(fourth_value, fifth_value)
+# The result for the first set
+first_result = first_find_highest(first_value, sec_value, third_value)
 
 # The final result or the highest number
-final_result = final_find_highest(res1, res2)
+final_result = final_most_highest(first_result, fourth_value, fifth_value)
 
 # Print the highest number
 print("The highest number is:", final_result)
