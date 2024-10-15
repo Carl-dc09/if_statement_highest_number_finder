86ffee8daf06c647fee8351ce9b04c271edd970f
unknown <diamantecarl9@gmail.com>
2024-10-15 18:03:05 +0800

    removed some and added some

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -1,18 +1,17 @@
-# Set the input numbers
-print("Input the values.")
 
-first_num_value = input("First value:")
-sec_num_value = input("Second value:")
-third_num_value = input("Third value:")
-fourth_num_value = input("Fourth value:")
-fifth_num_value = input("Fifth value:")
-
-def fing_highest(first_num_value, sec_num_value, third_num_value, fourth_num_value, fifth_num_value):
+def first_find_highest(first_num_value, sec_num_value, third_num_value):
     if first_num_value > sec_num_value:
         if first_num_value > third_num_value:
-            return "first_num_value"
+            return first_num_value
         else:
-            return "third_num_value"
+            return third_num_value
     else:
         if sec_num_value > third_num_value:
-            return "sec_num_value"
+            return sec_num_value
+        else:
+            return third_num_value
+
+
+
+
+
