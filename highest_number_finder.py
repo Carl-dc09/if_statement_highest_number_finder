cac006f5781d76e2aa904b56e8ef8e9f2fc06536
unknown <diamantecarl9@gmail.com>
2024-10-15 18:21:32 +0800

    finished the last def

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -20,6 +20,9 @@ def second_find_highest(fourth_num_value, fifth_num_value):
 def final_find_highest(first_highest, second_highest):
     if first_highest > second_highest:
         return first_highest
+    else:
+        return second_highest
+
 
 
 
